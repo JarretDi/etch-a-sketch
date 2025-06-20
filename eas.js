458c79b430915029f94ed9791486e2aa82830000
Jarret Diep <jarrettcdi@gmail.com>
2025-06-20 16:03:52 -0600

    add change grid size functionality
    -changed header to button in index.html
    -modified grid to not be hardcoded to 16x16
    -modified styles to also no be hardcoded

--- eas.js
+++ eas.js
@@ -1,11 +1,25 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
-for (let i = 0; i < 16 * 16; i++) {
-    const square = document.createElement("div");
-    square.setAttribute("class", "square");
-    container.appendChild(square);
+generateGrid(16);
+
+function generateGrid(dim) {
+    dim = Math.min(dim, 100);
+
+    container.innerHTML = '';
+
+    for (let i = 0; i < dim * dim; i++) {
+        const square = document.createElement("div");
+        square.setAttribute("class", "square");
+        square.style.flexBasis = (100/dim) + "%";
+        container.appendChild(square);
+    }
 }
 
 container.addEventListener("mouseover", (e) => {
     e.target.classList.add("painted");
 });
+
+button.addEventListener("click", () => {
+    generateGrid(+prompt("What size grid would you like? (max 100)", "16"));
+})
