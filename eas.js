14a57a45c5dbf9bf40b05cdbda4e6eb7eb8a3eca
Jarret Diep <jarrettcdi@gmail.com>
2025-06-20 15:18:31 -0600

    add draw functionality to grid
    -add event listener to container in eas.js
    -add painted class to styles.css

--- eas.js
+++ eas.js
@@ -3,7 +3,9 @@ const container = document.querySelector(".container");
 for (let i = 0; i < 16 * 16; i++) {
     const square = document.createElement("div");
     square.setAttribute("class", "square");
-    
-
     container.appendChild(square);
 }
+
+container.addEventListener("mouseover", (e) => {
+    e.target.classList.add("painted");
+});
